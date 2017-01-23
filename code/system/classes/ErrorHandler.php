99d39e63cecc6243cd84b9a8c3ca932e32c5c145
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2017-01-23 01:58:56 +0100

    ErrorHandler updated. — Formatting of HTML code corrected. — Class do not log error and do not try load configuration, when error occours outside normal CMS mode.

--- code/system/classes/ErrorHandler.php
+++ code/system/classes/ErrorHandler.php
@@ -43,22 +43,23 @@ trait ErrorHandler
 
 	static private function _addToLog($exception)
 	{
-		if (defined(__NAMESPACE__.'\\CONFIG_DIR')) {
-			// CONFIG_DIR can be not defined, when ErrorHandler is ran outside normal CMS code (in tests or utility scripts).
-			file_put_contents(
-				CONFIG_DIR . '/errors.log', (
-					"\n" . date('Y-m-d H:i') . '  ~~~~~~~~~~~~~~~~~~~~~~~~~~' .
-					"\nType:    " . ( ($exception instanceof \ErrorException)
-						? self::_getPHPErrorName($exception->getSeverity())
-						: get_class($exception) . ((empty($exception->getCode()))?'':' #'.$exception->getCode())
-					) .
-					"\nMessage: " . $exception->getMessage() .
-					"\nFile:    " . $exception->getFile() .
-					"\nLine:    " . $exception->getLine() .
-					"\nTrace: \n" . $exception->getTraceAsString() . "\n\n"
-				), FILE_APPEND
-			);
+		// Error should not be logged, when ErrorHandler is ran outside normal CMS mode (in unit tests, in utility scripts or when CMS is not installed).
+		if (!defined(__NAMESPACE__.'\\INIT') or !file_exists(CONFIG_DIR)) {
+			return;
 		}
+		file_put_contents(
+			CONFIG_DIR . '/errors.log', (
+				"\n" . date('Y-m-d H:i') . '  ~~~~~~~~~~~~~~~~~~~~~~~~~~' .
+				"\nType:    " . ( ($exception instanceof \ErrorException)
+					? self::_getPHPErrorName($exception->getSeverity())
+					: get_class($exception) . ((empty($exception->getCode()))?'':' #'.$exception->getCode())
+				) .
+				"\nMessage: " . $exception->getMessage() .
+				"\nFile:    " . $exception->getFile() .
+				"\nLine:    " . $exception->getLine() .
+				"\nTrace: \n" . $exception->getTraceAsString() . "\n\n"
+			), FILE_APPEND
+		);
 	}
 
 	static private function _printAsPlainText($exception)
@@ -76,60 +77,65 @@ trait ErrorHandler
 
 	static private function _printAsHTML($exception)
 	{
-		if (defined(__NAMESPACE__.'\\CONFIG_DIR') and !Settings::get('systemShowErrors')) {
-			?><!doctype html><meta charset="utf-8">
-<style>
-	html, body { margin: 0; padding: 0; height: 100%; width: 100%; }
-	div.wtHiErr { color: #777; font: 30px sans-serif; display: table; width: 100%; height: 100%; }
-	div.wtHiErr > div { display: table-cell; vertical-align: middle; text-align: center; }
-	div.wtHiErr span { display: block; font-size: 300px; color: #000; }
-	@media (max-height: 500px), (max-width: 400px) {
-		div.wtHiErr span { font-size: 200px; }
-	}
-</style>
-<div class="wtHiErr"><div>
-	<p><span role="presentation">&#9785;</span>Przepraszamy za usterki.</p>
-	<!-- <?= get_class($exception) . ($exception->getCode() ? ' #'.$exception->getCode() : '') ?> -->
-</div></div>
+		?><!doctype html><meta charset="utf-8"><?php
+
+		// Show full error information, when error occurs outside normal CMS mode or when detailed errors are enabled.
+		// Otherwise print only "sad face" message.
+		if (!defined(__NAMESPACE__.'\\INIT') or !file_exists(CONFIG_DIR) or Settings::get('systemShowErrors')) {
+			?>
+			<style>
+				html, body { margin: 0; padding: 0; height: 100%; width: 100%; }
+				div.wtFaErr { color: #000; font: 16px /1.5em sans-serif; display: table; height: 100%; width: 100%; background: rgba( 0,0,0, 0.3 ); }
+				div.wtFaErr > div { display: table-cell; vertical-align: middle; }
+				div.wtFaErr section { border: 1px solid red; max-width: 700px; margin: auto; padding: 0.4em 1em; background: #fff; box-shadow: 0 0 9px rgba( 0,0,0, 0.4 ); }
+				div.wtFaErr h1 { font-size: 1.7em; margin-bottom: -0.4em; color: #ff0000; }
+				div.wtFaErr dt { float: left; clear: both; width: 20%; opacity: 0.5; }
+				div.wtFaErr dd { float: left; width: 80%; margin-left: 0; }
+				div.wtFaErr dt:last-of-type { display: none; }
+				div.wtFaErr dd:last-of-type { clear: both; float: none; width: 100%; padding-top: 0.4em; }
+				div.wtFaErr #trace:not(:target), div.wtFaErr #trace:target + a { display: none; }
+				div.wtFaErr a { padding: 0.1em 1.5em; background: #eee; color: inherit; }
+			</style>
+			<div class="wtFaErr"><div><section>
+				<h1>Fatal error — WizyTówka <?= VERSION ?></h1>
+				<p>System encountered fatal error and execution must be interrupted.</p>
+				<dl>
+					<dt>Type</dt>
+					<dd><?= ( ($exception instanceof \ErrorException)
+							? self::_getPHPErrorName($exception->getSeverity())
+							: get_class($exception) . ((empty($exception->getCode()))?'':' #'.$exception->getCode())
+						) ?></dd>
+					<dt>Message</dt>
+					<dd><?= htmlspecialchars($exception->getMessage()) ?></dd>
+					<dt>File</dt>
+					<dd><?= $exception->getFile() ?></dd>
+					<dt>Line</dt>
+					<dd><?= $exception->getLine() ?></dd>
+					<dt>Trace</dt>
+					<dd>
+						<span id="trace"><?= nl2br(htmlspecialchars($exception->getTraceAsString())) ?></span>
+						<a href="#trace">Show trace…</a>
+					</dd>
+				</dl>
+			</section></div></div>
 			<?php
 		}
 		else {
-			?><!doctype html><meta charset="utf-8">
-<style>
-	html, body { margin: 0; padding: 0; height: 100%; width: 100%; }
-	div.wtFaErr { color: #000; font: 16px /1.5em sans-serif; display: table; height: 100%; width: 100%; background: rgba( 0,0,0, 0.3 ); }
-	div.wtFaErr > div { display: table-cell; vertical-align: middle; }
-	div.wtFaErr section { border: 1px solid red; max-width: 700px; margin: auto; padding: 0.4em 1em; background: #fff; box-shadow: 0 0 9px rgba( 0,0,0, 0.4 ); }
-	div.wtFaErr h1 { font-size: 1.7em; margin-bottom: -0.4em; color: #ff0000; }
-	div.wtFaErr dt { float: left; clear: both; width: 20%; opacity: 0.5; }
-	div.wtFaErr dd { float: left; width: 80%; margin-left: 0; }
-	div.wtFaErr dt:last-of-type { display: none; }
-	div.wtFaErr dd:last-of-type { clear: both; float: none; width: 100%; padding-top: 0.4em; }
-	div.wtFaErr #trace:not(:target), div.wtFaErr #trace:target + a { display: none; }
-	div.wtFaErr a { padding: 0.1em 1.5em; background: #eee; color: inherit; }
-</style>
-<div class="wtFaErr"><div><section>
-	<h1>Fatal error — WizyTówka <?= VERSION ?></h1>
-	<p>System encountered fatal error and execution must be interrupted.</p>
-	<dl>
-		<dt>Type</dt>
-		<dd><?= ( ($exception instanceof \ErrorException)
-				? self::_getPHPErrorName($exception->getSeverity())
-				: get_class($exception) . ((empty($exception->getCode()))?'':' #'.$exception->getCode())
-			) ?></dd>
-		<dt>Message</dt>
-		<dd><?= htmlspecialchars($exception->getMessage()) ?></dd>
-		<dt>File</dt>
-		<dd><?= $exception->getFile() ?></dd>
-		<dt>Line</dt>
-		<dd><?= $exception->getLine() ?></dd>
-		<dt>Trace</dt>
-		<dd>
-			<span id="trace"><?= nl2br(htmlspecialchars($exception->getTraceAsString())) ?></span>
-			<a href="#trace">Show trace…</a>
-		</dd>
-	</dl>
-</section></div></div><?php
+			?>
+			<style>
+				html, body { margin: 0; padding: 0; height: 100%; width: 100%; }
+				div.wtHiErr { color: #777; font: 30px sans-serif; display: table; width: 100%; height: 100%; }
+				div.wtHiErr > div { display: table-cell; vertical-align: middle; text-align: center; }
+				div.wtHiErr span { display: block; font-size: 300px; color: #000; }
+				@media (max-height: 500px), (max-width: 400px) {
+					div.wtHiErr span { font-size: 200px; }
+				}
+			</style>
+			<div class="wtHiErr"><div>
+				<p><span role="presentation">&#9785;</span>Przepraszamy za usterki.</p>
+				<!-- <?= get_class($exception) . ($exception->getCode() ? ' #'.$exception->getCode() : '') ?> -->
+			</div></div>
+			<?php
 		}
 	}
 
--- tests/ErrorHandlerTest.php
+++ tests/ErrorHandlerTest.php
@@ -7,34 +7,12 @@ class ErrorHandlerTest extends PHPUnit\Framework\TestCase
 {
 	static private $_errorLogPath;
 
-	static public function setUpBeforeClass()
-	{
-		self::$_errorLogPath = getcwd() . '/' . WizyTowka\CONFIG_DIR . '/errors.log';
-
-		if (file_exists(self::$_errorLogPath)) {
-			rename(self::$_errorLogPath, self::$_errorLogPath.'.bak');
-		}
-	}
-
-	static public function tearDownAfterClass()
-	{
-		if (file_exists(self::$_errorLogPath)) {
-			unlink(self::$_errorLogPath);
-		}
-		if (file_exists(self::$_errorLogPath.'.bak')) {
-			rename(self::$_errorLogPath.'.bak', self::$_errorLogPath);
-		}
-	}
-
 	public function testErrorHandler()
 	{
 		$exceptionMessage = 'Example exception #' . rand(100,999);
 		$this->expectOutputRegex('/'.$exceptionMessage.'/');
 
 		WizyTowka\ErrorHandler::handleException(new Exception($exceptionMessage, 8));
-
-		$errorLog = file_get_contents(self::$_errorLogPath);
-		$this->assertContains($exceptionMessage, $errorLog);
 	}
 
 	/**
