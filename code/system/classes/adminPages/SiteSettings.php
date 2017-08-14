347404d09a6fc61de03fa248159309dc57f3703b
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2017-08-14 23:35:16 +0200

    Website settings panel admin page updated.
    - PHP 5.6 compatibility issue fixed.
    - "Date/time format" field works.

--- code/system/classes/adminPages/SiteSettings.php
+++ code/system/classes/adminPages/SiteSettings.php
@@ -14,6 +14,19 @@ class SiteSettings extends WT\AdminPanel
 
 	private $_settings;
 
+	private $_definedDateFormats = [
+		'%Y-%m-%d %H:%M:%S',
+		'%Y-%m-%d %H:%M',
+		'%Y-%m-%d %k:%M',
+		'%d.%m.%Y %H:%M',
+		'%e.%m.%Y %H:%M',
+		'%e.%m.%Y %k:%M',
+		'%e %B %Y, %k:%M',
+		'%A, %e %B %Y, %k:%M',
+		'%e %B %Y, %A, %k:%M',
+		'%m/%d/%y %H:%M',
+	];
+
 	protected function _prepare()
 	{
 		$this->_settings = WT\Settings::get();
@@ -29,9 +42,8 @@ class SiteSettings extends WT\AdminPanel
 		}
 
 		// Try to update ".htaccess" file, when pretty links setting was changed.
-		// Give information about problem to user, when it is not possible.
-		if ($this->_settings->websitePrettyLinks != isset($_POST['websitePrettyLinks'])
-			and !$this->_updateHtaccess()) {
+		// Give information about problem to user if it is not possible.
+		if ($this->_settings->websitePrettyLinks != isset($_POST['websitePrettyLinks']) and !$this->_updateHtaccess()) {
 			$this->_apMessageError = true;
 			$this->_apMessage = 'Zmiany zostały zapisane. Przyjazne odnośniki wymagają ręcznej konfiguracji serwera.';
 		}
@@ -43,8 +55,12 @@ class SiteSettings extends WT\AdminPanel
 		$this->_settings->websiteHomepageId   = $_POST['websiteHomepageId'];
 
 		$this->_settings->websiteEmailAddress = $_POST['websiteEmailAddress'];
-		$this->_settings->websiteDateFormat   = $_POST['websiteDateFormat'];
 		$this->_settings->websitePrettyLinks  = isset($_POST['websitePrettyLinks']);
+
+		// Date/time format can be changed in configuration file. In this case form field will be disabled.
+		if (isset($_POST['websiteDateFormat']) and in_array($_POST['websiteDateFormat'], $this->_definedDateFormats)) {
+			$this->_settings->websiteDateFormat   = $_POST['websiteDateFormat'];
+		}
 
 		// Title pattern must have place for page title "%s".
 		if (mb_strpos($this->_settings->websiteTitlePattern, '%s') === false) {
@@ -58,14 +74,34 @@ class SiteSettings extends WT\AdminPanel
 
 	protected function _output()
 	{
-		$pages = WT\Page::getAll();
-		$this->_apTemplate->pagesIds = array_combine(array_column($pages, 'id'), array_column($pages, 'title'));
-
-		$this->_apTemplate->dateFormats = [
-			'Jeszcze niezaimplementowane',
-		];
-
 		$this->_apTemplate->settings = $this->_settings;
+
+		// PHP 5.6 backwards compatibility for using objects in array_columns() function.
+		// More here: http://php.net/manual/en/function.array-column.php#refsect1-function.array-column-changelog
+		$array_column = function($input, $column_key, $index_key){
+			if (PHP_VERSION_ID >= 70000) {
+				return array_column($input, $column_key, $index_key);
+			}
+			$array = [];
+			foreach ($input as $object) {
+				$array[$object->$index_key] = $object->$column_key;
+			}
+			return $array;
+		};
+
+		// "Website homepage" field — titles of public pages.
+		$pages = WT\Page::getAll();
+		$this->_apTemplate->pagesIds = $array_column($pages, 'title', 'id');
+
+		// "Date/time format" field — list with formats and examples.
+		$dateFormatsAndExamples = [];
+		foreach ($this->_definedDateFormats as $format) {
+			$dateFormatsAndExamples[$format] = (new WT\Text(1472711447))->formatAsDateTime($format)->get();
+		}
+		$this->_apTemplate->dateFormatsAndExamples = $dateFormatsAndExamples;
+
+		// "Date/time format" field — disable if setting was changed in configuration file.
+		$this->_apTemplate->disableDateFormatField = !in_array($this->_settings->websiteDateFormat, $this->_definedDateFormats);
 	}
 
 	private function _updateHtaccess()
