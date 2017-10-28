4540f65ca86936ec239064e7344bcf856772c7b2
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2017-10-28 23:30:38 +0200

    Website controller updated.
    - Controller initializes WebsiteRenderer internally.
    - Controller uses content type API to generate page content.

--- code/system/classes/Website.php
+++ code/system/classes/Website.php
@@ -2,22 +2,62 @@
 
 /**
 * WizyTówka 5
-* Website controller.
+* Website controller (uses content type API).
 */
 namespace WizyTowka;
 
 class Website extends Controller
 {
 	private $_page;
+	private $_contentTypeAPIBoxes = [];
+
+	private $_HTMLTemplate;
+	private $_renderer;
 
 	public function __construct()
 	{
+		// Get current page. If isn't specified, use home page.
 		$this->_page = !empty($_GET['id']) ? Page::getBySlug($_GET['id']) : Page::getById(Settings::get('websiteHomepageId'));
+
+		// Initialize HTML template.
+		$this->_HTMLTemplate = new HTMLTemplate;
+
+		// Get page boxes and its contents. Initialize content types.
+		foreach (PageBox::getAll($this->_page->id) as $pageBox) {
+			if (!$contentType = ContentType::getByName($pageBox->type)) {
+				throw WebsiteException::contentTypeNotExists($pageBox->type);
+			}
+
+			$contentTypeAPI = $contentType->initWebsitePageBox();
+			$contentTypeAPI->setPageData($pageBox->contents, $pageBox->settings);
+			$this->_contentTypeAPIBoxes[] = $contentTypeAPI;
+		}
+
+		// Initialize website renderer, which will prepare HTML template.
+		$this->_renderer = new WebsiteRenderer($this->_page, $this->_contentTypeAPIBoxes, $this->_HTMLTemplate);
+	}
+
+	public function POSTQuery()
+	{
+		foreach ($this->_contentTypeAPIBoxes as $box) {
+			try {
+				$box->POSTQuery();
+				// ContentTypeAPI::POSTQuery() throws an exception if content type does not support POST queries.
+				// Normally it's good, here this behavior is unwanted.
+			} catch (ContentTypeAPIException $e) {}
+		}
 	}
 
 	public function output()
 	{
-		echo $this->_page->title;
+		foreach ($this->_contentTypeAPIBoxes as $box) {
+			$box->HTMLContent();
+		}
+
+		// ContentTypeAPI::HTMLContent() must be called before template preparing by WebsiteRenderer.
+		$this->_renderer->prepareTemplate();
+
+		$this->_HTMLTemplate->render();
 	}
 
 	static public function URL($target, array $arguments = [])
@@ -42,5 +82,13 @@ class Website extends Controller
 		}
 
 		return Settings::get('websiteAddress') . ($pretty ? '/'.$slug : '/') . ($arguments ? '?'.http_build_query($arguments) : '');
+	}
+}
+
+class WebsiteException extends Exception
+{
+	static public function contentTypeNotExists($name)
+	{
+		return new self('Content type "' . $name . '" does not exists.', 1);
 	}
 }
