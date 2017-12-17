b3f3610bc76d1934b8d4b7e0be29ba9b940e15fe
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2017-12-17 04:58:15 +0100

    Database schema updated.
    - Small optimizations of columns types for MySQL.
    - Fix install problem in PostgreSQL (wrong tables order).
    - Fix inconsistent behavior between MySQL and other DBMSes in user name, case sensitive is required.

--- code/system/defaults/schema.sql
+++ code/system/defaults/schema.sql
@@ -1,42 +1,35 @@
-
 -- Database tables schema of WizyTówka CMS.
--- "wt_dbms" symbol means SQL lines adequate only for specified DBMS.
+-- This file is an template for real schema and it should be parsed before using.
+-- "wt_dbms" symbol means SQL lines used only for specified DBMS.
+
+ALTER DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; -- wt_dbms: mysql
 
 CREATE TABLE Users (
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, -- wt_dbms: sqlite
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,         -- wt_dbms: mysql
-	id SERIAL PRIMARY KEY,                         -- wt_dbms: pgsql
-	name VARCHAR(100) NOT NULL UNIQUE,
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,  -- wt_dbms: sqlite
+	id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT, -- wt_dbms: mysql
+	id SERIAL PRIMARY KEY,                          -- wt_dbms: pgsql
+	name VARCHAR(100) NOT NULL UNIQUE COLLATE utf8mb4_bin, -- wt_dbms: mysql  -- Force case sensitive.
+	name VARCHAR(100) NOT NULL UNIQUE,                     -- wt_dbms: ! mysql
 	password VARCHAR(100) NOT NULL,
 	permissions SMALLINT NOT NULL DEFAULT 0,
-	createdTime BIGINT NOT NULL DEFAULT 0
+	createdTime INTEGER UNSIGNED NOT NULL DEFAULT 0 -- wt_dbms: mysql
+	createdTime BIGINT NOT NULL DEFAULT 0           -- wt_dbms: ! mysql
 );
 
 CREATE TABLE Files (
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, -- wt_dbms: sqlite
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,         -- wt_dbms: mysql
-	id SERIAL PRIMARY KEY,                         -- wt_dbms: pgsql
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,  -- wt_dbms: sqlite
+	id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT, -- wt_dbms: mysql
+	id SERIAL PRIMARY KEY,                          -- wt_dbms: pgsql
 	name VARCHAR(100) NOT NULL UNIQUE,
 	userId INTEGER REFERENCES Users(id) ON DELETE SET NULL,
-	uploadedTime BIGINT NOT NULL DEFAULT 0
-);
-
-CREATE TABLE PageBoxes (
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, -- wt_dbms: sqlite
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,         -- wt_dbms: mysql
-	id SERIAL PRIMARY KEY,                         -- wt_dbms: pgsql
-	contentType VARCHAR(50) NOT NULL,
-	contents TEXT NOT NULL,
-	settings TEXT NOT NULL,
-	pageId INTEGER NOT NULL REFERENCES Pages(id) ON DELETE CASCADE,
-	positionRow SMALLINT NOT NULL,
-	positionColumn SMALLINT NOT NULL
+	uploadedTime INTEGER UNSIGNED NOT NULL DEFAULT 0 -- wt_dbms: mysql
+	uploadedTime BIGINT NOT NULL DEFAULT 0           -- wt_dbms: ! mysql
 );
 
 CREATE TABLE Pages (
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, -- wt_dbms: sqlite
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,         -- wt_dbms: mysql
-	id SERIAL PRIMARY KEY,                         -- wt_dbms: pgsql
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,  -- wt_dbms: sqlite
+	id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT, -- wt_dbms: mysql
+	id SERIAL PRIMARY KEY,                          -- wt_dbms: pgsql
 	slug VARCHAR(100) NOT NULL UNIQUE,
 	title VARCHAR(500) NOT NULL,
 	titleHead VARCHAR(500),
@@ -44,10 +37,24 @@ CREATE TABLE Pages (
 	keywords VARCHAR(500),
 	isDraft SMALLINT NOT NULL CHECK(isDraft IN (0,1)),
 	userId INTEGER REFERENCES Users(id) ON DELETE SET NULL,
-	updatedTime BIGINT NOT NULL DEFAULT 0,
-	createdTime BIGINT NOT NULL DEFAULT 0
+	updatedTime INTEGER UNSIGNED NOT NULL DEFAULT 0, -- wt_dbms: mysql
+	updatedTime BIGINT NOT NULL DEFAULT 0,           -- wt_dbms: ! mysql
+	createdTime INTEGER UNSIGNED NOT NULL DEFAULT 0 -- wt_dbms: mysql
+	createdTime BIGINT NOT NULL DEFAULT 0           -- wt_dbms: ! mysql
 );
 CREATE INDEX Pages_isDraft ON Pages(isDraft);
+
+CREATE TABLE PageBoxes (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,  -- wt_dbms: sqlite
+	id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT, -- wt_dbms: mysql
+	id SERIAL PRIMARY KEY,                          -- wt_dbms: pgsql
+	contentType VARCHAR(50) NOT NULL,
+	contents TEXT NOT NULL,
+	settings TEXT NOT NULL,
+	pageId INTEGER NOT NULL REFERENCES Pages(id) ON DELETE CASCADE,
+	positionRow SMALLINT NOT NULL,
+	positionColumn SMALLINT NOT NULL
+);
 
 -- Workaround for Pages.isDraft. MySQL have not support of CHECK constraint.
 CREATE TRIGGER Pages_isDraft_INSERT BEFORE INSERT ON Pages FOR EACH ROW -- wt_dbms: mysql
